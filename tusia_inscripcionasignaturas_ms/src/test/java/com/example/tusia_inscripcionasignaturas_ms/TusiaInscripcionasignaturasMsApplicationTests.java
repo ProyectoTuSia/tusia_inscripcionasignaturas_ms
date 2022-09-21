ce6c36461c82da45cf78dfa7838e874ad3eaee85
scassiano <scassiano@unal.edu.co>
2022-09-21 12:34:10 -0500

    Creacion del proyecto en spring y propiedades aplicacion

---
+++ tusia_inscripcionasignaturas_ms/src/test/java/com/example/tusia_inscripcionasignaturas_ms/TusiaInscripcionasignaturasMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tusia_inscripcionasignaturas_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TusiaInscripcionasignaturasMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
